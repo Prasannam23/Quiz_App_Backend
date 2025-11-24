386562f270c0ea8da5e158f68de619c44d7fbd76
Prasannam Tiwari <prasannamtiwari23@gmail.com>
2025-11-24 23:18:06 +0530

    fix

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,6 +1,5 @@
-import cluster from 'cluster';
-import os from 'os';
-import { initializeWorkerApp } from './app'; 
+import cluster from "cluster";
+import os from "os";
 
 const numCPUs = os.cpus().length;
 
@@ -8,17 +7,16 @@ if (cluster.isPrimary) {
   console.log(` Primary process ${process.pid} is running`);
   console.log(` Forking ${numCPUs} workers...\n`);
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker,) => {
+  cluster.on("exit", (worker) => {
     console.log(` Worker ${worker.process.pid} died. Restarting...`);
     cluster.fork();
   });
 
 } else {
-  initializeWorkerApp().catch(err => {
-    console.error(`Worker ${process.pid} failed to initialize:`, err);
-  });
+  // Start the application inside each worker
+  require("./app");
 }
