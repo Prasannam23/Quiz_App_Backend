0cb3563e04bad6122734fa4a6bd19002d98c4ef3
Prasannam Tiwari <prasannamtiwari23@gmail.com>
2025-11-25 05:54:09 +0530

    fixed google auth

--- src/google/strategies/google.ts
+++ src/google/strategies/google.ts
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: 'http://localhost:8000/api/auth/google/callback',
+      callbackURL: 'https://quizappbackend.duckdns.org/api/auth/google/callback',
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
